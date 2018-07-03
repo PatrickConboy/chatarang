def1e54e3028b86719a8f5d9640bbcc2256fd55e
Unknown <rispir45@gmail.com>
2018-07-03 15:31:37 -0400

    Add messages to chat state

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
 
+import ChatHeader from './ChatHeader'
+import MessageList from './MessageList'
+import MessageForm from './MessageForm'
+
 class Chat extends Component {
+	constructor() {
+		super()
+
+		this.state = {
+			messages: [
+				{
+					id: 1,
+					user: {
+						uid: 'sdfsdf',
+						displayName: 'Patches',
+						email: 'rispir45@gmail.com'
+					},
+					body: 'Chatting up a storm, yo!',
+				},
+
+				{
+					id: 2,
+					user: {
+						uid: 'asdfsdf',
+						displayName: 'Dana',
+						email: 'dana@fretless.com'
+					},
+					body: 'This guy is so annoying. I hate my job.'
+				},
+			]
+		}
+	}
+
 	render() {
 		return (
 			<div className="Sidebar">
-				Chat
-            </div>
+				<ChatHeader />
+				<MessageList messages={this.state.messages} />
+				<MessageForm />
+			</div>
 		)
 	}
 }
