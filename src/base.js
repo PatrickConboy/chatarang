ee2affdbb9f01fea75c6875cacd13ea36f5d47ba
Unknown <rispir45@gmail.com>
2018-07-05 17:15:29 -0400

    Initalize firebase

---
+++ src/base.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import Rebase from 're-base
+import database from 'firebase/database'
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyCTX_AX7BVEeZlGuL0A6A6SWj46xI0Tdhg",
+  authDomain: "chatarang-5f8df.firebaseapp.com",
+  databaseURL: "https://chatarang-5f8df.firebaseio.com",
+  projectId: "chatarang-5f8df",
+  storageBucket: "",
+  messagingSenderId: "868857127017"
+};
+const app = firebase.initializeApp(config);
+const db = firebase.database(app)
+const base = Rebase.createClass(db)
