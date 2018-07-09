916f0147adcf59e60c2e34296344109e989cb528
Unknown <rispir45@gmail.com>
2018-07-09 15:18:21 -0400

    Github Login

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react'
 import { StyleSheet, css } from 'aphrodite'
 
-import { auth, googleProvider } from './base'
+import { auth, googleProvider, githubProvider } from './base'
 
 class SignIn extends Component {
   state = {
     email: '',
   }
 
-  authenticate = () => {
+  authenticate = (provider) => {
     auth
-      .signInWithPopup(googleProvider)
+      .signInWithPopup(provider)
       .then(result => {
-        const { user } = result
-        this.props.handleAuth({
-          displayName: user.displayName,
-          email: user.email,
-          uid: user.uid,
-        })
+        this.props.handleAuth(result.user)
       })
   }
 
@@ -48,6 +43,7 @@ class SignIn extends Component {
             className={css(styles.form)}
             onSubmit={this.handleSubmit}
           >
+            <h2>Sign In</h2>
             {/* <label
               htmlFor="email"
               className={css(styles.label)}
@@ -72,9 +68,19 @@ class SignIn extends Component {
             <button
               type="button"
               className={css(styles.button)}
-              onClick={this.authenticate}
+              onClick={() => this.authenticate(googleProvider)}
             >
+              <i className={`fab fa-google ${css(styles.brandIcon)}`}></i>
               Sign in with Google
+            </button>
+
+            <button
+              type="button"
+              className={css(styles.button, styles.github)}
+              onClick={() => this.authenticate(githubProvider)}
+            >
+              <i className={`fab fa-github ${css(styles.brandIcon)}`}></i>
+              Sign in with GitHub
             </button>
           </form>
 
@@ -89,7 +95,6 @@ class SignIn extends Component {
     )
   }
 }
-
 const styles = StyleSheet.create({
   signIn: {
     display: 'flex',
@@ -97,7 +102,6 @@ const styles = StyleSheet.create({
     height: '100vh',
     backgroundColor: '#f6f6f6',
   },
-
   header: {
     backgroundColor: '#fff',
     height: '4rem',
@@ -107,7 +111,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     boxShadow: '0 1px 1px rgba(0,0,0,.1)',
   },
-
   title: {
     color: '#ff3344',
     fontWeight: 400,
@@ -115,7 +118,6 @@ const styles = StyleSheet.create({
     lineHeight: '80px',
     fontSize: '2rem',
   },
-
   main: {
     flex: 1,
     textAlign: 'center',
@@ -123,23 +125,20 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     margin: '0 auto',
+    paddingBottom: '3rem',
   },
-
   form: {
     width: '40rem',
-    height: '15rem',
     backgroundColor: 'white',
     boxShadow: '0 1px 1px rgba(0,0,0,.1)',
     marginBottom: '2rem',
-    padding: '2rem 0 0',
+    paddingBottom: '2rem',
   },
-
   label: {
     display: 'block',
     textTransform: 'uppercase',
     color: '#999',
   },
-
   input: {
     width: '20rem',
     fontSize: '1.5rem',
@@ -149,19 +148,16 @@ const styles = StyleSheet.create({
     marginBottom: '1rem',
     textAlign: 'center',
     padding: '0.5rem',
-
     ':focus': {
       outline: 0,
     },
   },
-
   h2: {
     fontWeight: 'normal',
   },
-
   button: {
     display: 'block',
-    margin: '0 auto',
+    margin: '0 auto 1rem',
     padding: '1rem 2rem',
     fontSize: '1.2rem',
     borderRadius: '1rem',
@@ -169,7 +165,12 @@ const styles = StyleSheet.create({
     color: 'white',
     width: '20rem',
   },
+  github: {
+    marginBottom: 0,
+    backgroundColor: '#6e5494',
+  },
+  brandIcon: {
+    marginRight: '1rem',
+  },
 })
-
-
 export default SignIn
