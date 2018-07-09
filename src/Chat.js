6ec293eac84383b40a20f519a3526e7e74692a2d
Unknown <rispir45@gmail.com>
2018-07-09 16:29:23 -0400

    Following Davey's code...

--- src/Chat.js
+++ src/Chat.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import ChatHeader from './ChatHeader'
 import MessageList from './MessageList'
 import MessageForm from './MessageForm'
+
 import base from './base'
 
 class Chat extends Component {
@@ -12,6 +13,21 @@ class Chat extends Component {
     this.state = {
       messages: [],
     }
+  }
+
+  componentDidMount() {
+    this.messagesRef = base.syncState(
+      'messages/general',
+      {
+        context: this,
+        state: 'messages',
+        asArray: true,
+      }
+    )
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.messagesRef)
   }
 
   addMessage = (body) => {
@@ -25,23 +41,18 @@ class Chat extends Component {
     })
 
     this.setState({ messages })
-
-  }
-
-  componentDidMount() {
-    base.syncState('messages',{
-      context: this,
-      state: 'messages',
-      asArray: true,
-    })
   }
 
   render() {
     return (
-      <div className="Chat" style={styles}>
+      <div
+        className="Chat"
+        style={styles}
+      >
         <ChatHeader room={this.props.room} />
         <MessageList
           messages={this.state.messages}
+          room={this.props.room}
         />
         <MessageForm addMessage={this.addMessage} />
       </div>
